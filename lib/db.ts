cfd9538fbd8147e36d44bee3f8762f4c673d6419
KasunBandara921 <bandarakasun495@gmail.com>
2026-07-16 00:53:12 +0530

    update  prisma.config.ts and db link

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,16 @@
 import { PrismaClient } from '@prisma/client';
-// add  no-var
+import { PrismaNeon } from '@prisma/adapter-neon';
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
 
 /* eslint-disable no-var */
-
 declare global {
   var prisma: PrismaClient | undefined;
 }
+/* eslint-enable no-var */
 
-/* eslint-disable no-var */
-
-export const db = globalThis.prisma || new PrismaClient();
+export const db =
+  globalThis.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = db;
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,3 +1,4 @@
+import "dotenv/config"; // Loads variables from .env
 import path from "node:path";
 import { defineConfig } from "prisma/config";
 
@@ -5,6 +6,6 @@ export default defineConfig({
   earlyAccess: true,
   schema: path.join(__dirname, "prisma", "schema.prisma"),
   datasource: {
-    url: "postgresql://neondb_owner:npg_TVmshL4NFr8v@ep-blue-sun-a85qen4w.eastus2.azure.neon.tech/neondb?sslmode=require&connect_timeout=30",
+    url: process.env.DATABASE_URL,
   },
 });
